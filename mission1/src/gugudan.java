3fe59c70f427f34bc9f08dd601abed767913a513
PhilSoGooood <peelhw@gamil.com>
2021-11-02 17:50:44 +0900

    update scanner.close()

--- mission1/src/gugudan.java
+++ mission1/src/gugudan.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 public class gugudan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("시작단을 입력해주세요: ");
+        System.out.print("시작단을 입력해주세요: ");
         int a = sc.nextInt(); // 구구단 시작단
-        System.out.println("끝단을 입력해주세요: ");
+        System.out.print("끝단을 입력해주세요: ");
         int b = sc.nextInt(); // 구구단 끝단
         for(int i=a; i<=b; i++) {
             for(int j=1; j<=9; j++) {
                 System.out.println(i + "*" + j + "=" + i*j);
             }
         }
+        sc.close();
     }
 }
