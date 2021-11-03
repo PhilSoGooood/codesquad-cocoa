12e60f13236f4e22509a9e142919a29419f3d569
PhilSoGooood <peelhw@gamil.com>
2021-11-03 18:15:40 +0900

    update RandomMember

--- mission2/RandomMember/src/RandomMember.java
+++ mission2/RandomMember/src/RandomMember.java
@@ -4,6 +4,7 @@ class Member {
     int memberNumber = members.length;
     int inputNumber;
 
+    // 몇명을 뽑을지 input을 받는 method
     public int getInputNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("몇 명인가요?(1 ~ " + memberNumber + "): ");
@@ -16,27 +17,33 @@ class Member {
         return inputNumber;
     }
 
+    // 뽑을 인원만큼의 랜덤숫자(0 ~ 멤버숫자)를 리턴하는 method
     public int[] getRandomNumber(int inputNumber) {
         this.inputNumber = inputNumber;
         Random random = new Random();
         int array[] = new int[inputNumber];
+
         for(int i=0; i < inputNumber; i++) {
             array[i] = random.nextInt(memberNumber);
+            for(int j=0; j < i; j++){
+                if(array[i] == array[j]) {
+                    i--;
+                }
+            }
         }
         return array;
     }
-
-//public String pickMember()
-
 }
 
 
 public class RandomMember {
     public static void main(String[] args) {
-        Member m = new Member();
-        int n = m.getInputNumber();
-        int array[];
-        array = m.getRandomNumber(n);
-        System.out.println(array);
+        Member member = new Member();
+        int n = member.getInputNumber();
+        int randomNumber[];
+        randomNumber = member.getRandomNumber(n);
+        for (int i = 0; i < member.inputNumber; i++) {
+            System.out.print(member.members[randomNumber[i]] + " ");
+        }
     }
 }
