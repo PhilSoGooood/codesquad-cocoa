e1b7fcb8f8e208f0b71cfd8720758aefea2c17f0
PhilSoGooood <peelhw@gmail.com>
2021-11-06 23:55:09 +0900

    update BOJ11654

---
+++ BOJ/BOJ11654.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		char chr = scanner.next().charAt(0);
+		int intChr = chr;
+		System.out.println(intChr);
+	}
+}
