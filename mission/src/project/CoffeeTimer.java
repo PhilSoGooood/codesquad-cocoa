2d63be82a3905a076cb6855a9b9096bbe8a0696b
PhilSoGooood <peelhw@gmail.com>
2021-11-29 23:29:20 +0900

    V60Start 처음 물붓기 타이머 구현

--- mission/src/project/CoffeeTimer.java
+++ mission/src/project/CoffeeTimer.java
@@ -3,10 +3,12 @@ package project;
 public class CoffeeTimer {
 	public static void main(String[] args) {
 	//	커피 도구 선택 및 추가
-		CoffeeTool ct = new CoffeeTool();
-		ct.selectTool();
+	// 	CoffeeTool ct = new CoffeeTool();
+	// 	ct.selectTool();
 	//	원두량 입력 및 물 추출량 계산
 	//	디개싱 시간
+		V60Start start = new V60Start();
+		start.brewing();
 	//	본격 추출 시간 카운트
 	//	피드백
 	}
--- mission/src/project/V60Start.java
+++ mission/src/project/V60Start.java
@@ -1,9 +1,26 @@
 package project;
 import java.util.Scanner;
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 class V60Start {
 	public void brewing(){
-
+		Timer timer = new Timer();
+		TimerTask timerTask = new TimerTask() {
+			int count = 10;
+			@Override
+			public void run() {
+				if(count > 0){
+					System.out.printf("남은 시간 %d초%n", count);
+					count--;
+				}
+				else {
+					timer.cancel();
+					System.out.println("시간 종료.");
+				}
+			}
+		};
+		System.out.println("물 66ml를 평평하게 부어주세요.");
+		timer.schedule(timerTask, 1000, 1000);
 	}
 }
