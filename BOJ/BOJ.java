f4c971905758c05331c0be47c57b35b95ef98853
PhilSoGooood <peelhw@gmail.com>
2021-12-18 16:06:50 +0900

    solved BOJ10250

---
+++ BOJ/BOJ.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		for(int tc=0; tc<T; tc++){
+			st = new StringTokenizer(br.readLine());
+			int H = Integer.parseInt(st.nextToken());
+			int W = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int room = (int)Math.ceil((double)N / H);
+			int floor = (N-1) % H + 1;
+			System.out.printf("%d%02d%n", floor,room);
+		}
+		br.close();
+	}
+}
