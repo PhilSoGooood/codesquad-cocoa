f1ba823fc83516c66034eaec61e5da3bad3b7aed
PhilSoGooood <peelhw@gmail.com>
2021-12-31 01:35:22 +0900

    solved BOJ18870

---
+++ BOJ/BOJ18870.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		String[] input = br.readLine().split(" ");
+		int[] nums = new int[N];
+		for(int i=0; i<N; i++){
+			nums[i] = Integer.parseInt(input[i]);
+		}
+		int[] sortedNums = nums.clone();
+		Arrays.sort(sortedNums);
+
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int idx = 0;
+		for(int n : sortedNums){
+			if(!map.containsKey(n)){
+				map.put(n, idx++);
+			}
+		}
+		for(int n: nums){
+			bw.write(map.get(n)+" ");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
